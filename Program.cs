66beb640ee0a1570d51bc9a19eec8e65a91db226
Nivaldo Júnior <nivaldocruz777@gmail.com>
2024-03-24 07:52:47 -0300

    Pogram atualizado.

--- Program.cs
+++ Program.cs
@@ -3,14 +3,14 @@
 Smartphone nokia = new Nokia(numero: "1234-5678", modelo: "Nokia 1", imei: "11111111", memoria: 64);
 Smartphone iphone = new Iphone(numero: "0987-4321", modelo: "Iphone 1", imei: "22222222", memoria: 16);
 
-Console.WriteLine("Smartphone Nokia: ");
+Console.WriteLine($"Smartphone Nokia. Modelo: {nokia.Modelo}. Imei: {nokia.Imei}.");
 nokia.Ligar();
 nokia.ReceberLigacao();
 nokia.InstalarAplicativo("Whatsapp");
 
 Console.WriteLine("\n");
 
-Console.WriteLine("Smartphone Iphone: ");
+Console.WriteLine($"Smartphone Iphone. Modelo: {iphone.Modelo}. Imei: {iphone.Imei}.");
 iphone.Ligar();
 iphone.ReceberLigacao();
 iphone.InstalarAplicativo("Instagram");
